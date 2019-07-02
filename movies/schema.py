fcf1a37dd20927b7dda7e90acecd51d5174601c8
Vinh Huynh <vinh.huynhquang@asnet.com.vn>
2019-07-02 17:07:00 +0700

    using relay and django_filter to solve features paginations and filters

--- movies/schema.py
+++ movies/schema.py
@@ -1,5 +1,7 @@
 import graphene
+from graphene import relay
 from graphene_django.types import DjangoObjectType, ObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from movies.models import Actor, Movie
 
 #########################################################################
@@ -9,17 +11,30 @@ from movies.models import Actor, Movie
 class ActorType(DjangoObjectType):
     class Meta:
         model = Actor
+        filter_fields = {
+            'name': ['exact', 'istartswith',],
+        }
+        interfaces = (relay.Node, )
 
 class MovieType(DjangoObjectType):
     class Meta:
         model = Movie
+        filter_fields = {
+            'title': ['exact', 'istartswith', 'icontains',],
+            'year': ['exact'],
+        }
+        interfaces = (relay.Node, )
 
 # Next, continue creating the Query Type
 class Query(ObjectType):
     actor = graphene.Field(ActorType, id=graphene.Int())
     movie = graphene.Field(MovieType, id=graphene.Int())
-    actors = graphene.List(ActorType)
-    movies = graphene.List(MovieType)
+    # actors = graphene.List(ActorType)
+    # movies = graphene.List(MovieType)
+    # actor = relay.Node.Field(ActorType)
+    # movie = relay.Node.Field(MovieType)
+    actors = DjangoFilterConnectionField(ActorType)
+    movies = DjangoFilterConnectionField(MovieType)
 
     def resolve_actor(self, info, **kwargs):
         id = kwargs.get('id', None)
@@ -35,10 +50,10 @@ class Query(ObjectType):
             return Movie.objects.get(pk=id)
         return None
 
-    def resolve_actors(self, info):
+    def resolve_actors(self, info, **kwargs):
         return Actor.objects.all()
 
-    def resolve_movies(self, info):
+    def resolve_movies(self, info, **kwargs):
         return Movie.objects.all()
 
 #########################################################################
