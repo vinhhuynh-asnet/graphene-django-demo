cf74d819c20e073cc8e6ceebbe1a67a893101e6c
Vinh Huynh <vinh.huynhquang@asnet.com.vn>
2019-07-04 17:10:22 +0700

    fix error csrf

--- configs/urls.py
+++ configs/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 # Graphene
 from graphene_django.views import GraphQLView
+from graphql_jwt.decorators import jwt_cookie
 from configs.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
